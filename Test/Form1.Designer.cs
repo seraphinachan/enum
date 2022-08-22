57bb9da126789e24ef896c6cd7733b40edd82bbb
Administrator <Administrator@DESKTOP-P29IQKV>
2022-08-22 17:58:33 +0900

    초기 커밋.

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxName = new System.Windows.Forms.TextBox();
+            this.lboxDay = new System.Windows.Forms.ListBox();
+            this.lboxTime = new System.Windows.Forms.ListBox();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.btnResult2 = new System.Windows.Forms.Button();
+            this.tboxResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tboxName
+            // 
+            this.tboxName.Location = new System.Drawing.Point(12, 12);
+            this.tboxName.Name = "tboxName";
+            this.tboxName.Size = new System.Drawing.Size(100, 23);
+            this.tboxName.TabIndex = 0;
+            // 
+            // lboxDay
+            // 
+            this.lboxDay.FormattingEnabled = true;
+            this.lboxDay.ItemHeight = 15;
+            this.lboxDay.Location = new System.Drawing.Point(12, 58);
+            this.lboxDay.Name = "lboxDay";
+            this.lboxDay.Size = new System.Drawing.Size(172, 184);
+            this.lboxDay.TabIndex = 2;
+            // 
+            // lboxTime
+            // 
+            this.lboxTime.FormattingEnabled = true;
+            this.lboxTime.ItemHeight = 15;
+            this.lboxTime.Location = new System.Drawing.Point(190, 58);
+            this.lboxTime.Name = "lboxTime";
+            this.lboxTime.Size = new System.Drawing.Size(172, 184);
+            this.lboxTime.TabIndex = 3;
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(12, 259);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 23);
+            this.btnResult.TabIndex = 4;
+            this.btnResult.Text = "결과보기";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // btnResult2
+            // 
+            this.btnResult2.Location = new System.Drawing.Point(109, 259);
+            this.btnResult2.Name = "btnResult2";
+            this.btnResult2.Size = new System.Drawing.Size(121, 23);
+            this.btnResult2.TabIndex = 5;
+            this.btnResult2.Text = "StringFormat Test";
+            this.btnResult2.UseVisualStyleBackColor = true;
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Enabled = false;
+            this.tboxResult.Location = new System.Drawing.Point(12, 298);
+            this.tboxResult.Multiline = true;
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.Size = new System.Drawing.Size(350, 78);
+            this.tboxResult.TabIndex = 6;
+            this.tboxResult.Text = "    ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 393);
+            this.Controls.Add(this.tboxResult);
+            this.Controls.Add(this.btnResult2);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.lboxTime);
+            this.Controls.Add(this.lboxDay);
+            this.Controls.Add(this.tboxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox tboxName;
+        private ListBox lboxDay;
+        private ListBox lboxTime;
+        private Button btnResult;
+        private Button btnResult2;
+        private TextBox tboxResult;
+    }
+}
